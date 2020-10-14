abc3bf054c0944860ece11781d92536f72ab903a
Patrick Sullivan <patrick@MacBook-Pro.local>
2020-10-14 08:44:28 -0400

    section 2.10

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,46 @@
+/*
+console.log('Hello world!!!')
+
+var firstName = 'Patrick';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+
+/*
+variable mutation and type coercion
+*/
+/*
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+//type coercion
+var job, isMarried;
+
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' ' + job + ' ' + isMarried);
+
+//var mutation
+age = 'twenty eight';
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+/* Math operators */
+

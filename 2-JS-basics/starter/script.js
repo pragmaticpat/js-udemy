367cd9b90de1f01cbd6033e9fb4ef745b6af3f0b
Patrick Sullivan <patrick@MacBook-Pro.local>
2020-10-19 10:34:04 -0400

    completed coding challenge 5

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -277,33 +277,133 @@ console.log(firstName + ' ' + lastName);
  * coding challenge 3
  */
 
- var mark = {
-     fullName: 'Mark Smith',
-     mass: 100,
-     height: 1.90,
-     calcBMI: function(){
-         this.bmi = this.mass / this.height^2;
-         return this.bmi;
-     }
- }
+//  var mark = {
+//      fullName: 'Mark Smith',
+//      mass: 100,
+//      height: 1.90,
+//      calcBMI: function(){
+//          this.bmi = this.mass / this.height^2;
+//          return this.bmi;
+//      }
+//  }
 
- var john = {
-    fullName: 'John Smith',
-    mass: 90,
-    height: 1.75,
-    calcBMI: function(){
-        this.bmi = this.mass / this.height^2;
-        return this.bmi;
+//  var john = {
+//     fullName: 'John Smith',
+//     mass: 190,
+//     height: 1.75,
+//     calcBMI: function(){
+//         this.bmi = this.mass / this.height^2;
+//         return this.bmi;
+//     }
+// }
+
+// var name, bmi;
+// if(mark.calcBMI() > john.calcBMI()){
+//     console.log(mark.fullName + ' has the higher BMI of ' + mark.bmi);
+// } else if(john.bmi > mark.bmi){
+//     console.log(john.fullName + ' has the higher BMI of ' + john.bmi);
+// } else {
+//     console.log('They both have the same BMI of ' + mark.bmi);
+// }
+
+// console.log(mark, john);
+
+/**
+ * Loops and iteration
+ */
+
+//  for(var i = 0; i < 10; i++){
+//      console.log(i);
+//  }
+
+//  var john = ['John', 'Smith', 1990, 'designer','blue'];
+// //  var i = 0;
+// //  while(i < john.length){
+// //      if(typeof john[i] !== 'string') continue;
+// //      console.log(john[i++]);
+// //  }
+
+//  for(var i = 0; i < john.length; i++){
+//      if(typeof john[i] !== 'string') continue;
+//      console.log(john[i]);
+//  }
+
+
+//  for(var i = john.length-1; i >= 0; i--){
+//     if(typeof john[i] !== 'string') continue;
+//     console.log(john[i]);
+// }
+
+/** Coding Challenge 5 */
+
+var tipCalculatorJohn = {
+    familyName: 'John',
+    bills: [124, 48, 268, 180, 42],
+    tips: [],
+    totals: [],
+    calculateTips: function(){
+        var tip = 0;
+        for(var i = 0; i < this.bills.length; i++){
+            if(this.bills[i] < 50) { 
+                tip = .2; 
+            } else if(this.bills[i] >= 50 && this.bills[i] <=200 ) { 
+                tip = .15; 
+            } else { tip = .1; }
+
+            this.tips[i] = tip * this.bills[i];
+            this.totals[i] = this.bills[i] + this.tips[i];
+        }
+
+        console.log(this.bills, this.tips, this.totals);
     }
 }
 
-var name, bmi;
-if(mark.calcBMI() > john.calcBMI()){
-    console.log(mark.fullName + ' has the higher BMI of ' + mark.bmi);
-} else if(john.bmi > mark.bmi){
-    console.log(john.fullName + 'has the higher BMI of ' + john.bmi);
-} else {
-    console.log('They both have the same BMI of ' + mark.bmi);
+tipCalculatorJohn.calculateTips();
+
+var tipCalculatorMark = {
+    familyName: 'Mark',
+    bills: [77, 475, 110, 45],
+    tips: [],
+    totals: [],
+    calculateTips: function(){
+        var tip = 0;
+        for(var i = 0; i < this.bills.length; i++){
+            if(this.bills[i] < 100) { 
+                tip = .2; 
+            } else if(this.bills[i] >= 100 && this.bills[i] <= 300) { 
+                tip = .15; 
+            } else { tip = .25; }
+
+            this.tips[i] = tip * this.bills[i];
+            this.totals[i] = this.bills[i] + this.tips[i];
+        }
+
+        console.log(this.bills, this.tips, this.totals);
+    }
 }
 
-console.log(mark, john);
+tipCalculatorMark.calculateTips();
+
+// Extra Credit
+
+function getAverageTips(tipsArray){
+    var sum = 0;
+    for (var i = 0; i < tipsArray.length; i++){
+        sum += tipsArray[i];
+    }
+
+    return sum / tipsArray.length;
+}
+
+var averageTipsForJohn = getAverageTips(tipCalculatorJohn.tips);
+var averageTipsForMark = getAverageTips(tipCalculatorMark.tips);
+
+console.log(averageTipsForJohn, averageTipsForMark);
+
+if(averageTipsForJohn > averageTipsForMark) {
+    console.log( 'John\'s family has higher average tip of ' + averageTipsForJohn);
+} else if (averageTipsForMark > averageTipsForJohn){
+    console.log( 'Mark\'s family has higher average tip of ' + averageTipsForMark);
+} else {
+    console.log('It\s a tie! Both families averaged tips of ' + averageTipsForJohn);
+}

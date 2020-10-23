6eafb1bce78ad90b6a90a666484c2ad70b9d0d1c
Patrick Sullivan <patrick@MacBook-Pro.attlocal.net>
2020-10-23 09:58:20 -0400

    finished basics

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,15 +1,32 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+// functions
+calculateAge(1965);
+
+function calculateAge(year){
+    console.log(2020 - year);
+}
+
+// retirement(1990); won't work here, because hoisting doesn't work for function expressions
+var retirement = function(year){
+    console.log(65 - (2020 - year));
+}
+
+// variables
+
+console.log(age); //undefined, variable hoisting
+var age = 23; 
+console.log(age);
 
 
-
-
-
-
-
-
-
+function foo(){
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 
@@ -67,11 +84,37 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+// // console.log(this);
+// calculateAge(1985);
 
+// function calculateAge(year){
+//     console.log(2020-year);
+//     console.log(this);
+// }
 
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function(){
+        console.log(this);
+        console.log(2020-this.yearOfBirth);
 
+        // function innerFunction(){
+        //     console.log(this);
+        // }
 
+        // innerFunction();
 
+    }
+}
 
+john.calculateAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge; //method borrowing
+mike.calculateAge();
 
